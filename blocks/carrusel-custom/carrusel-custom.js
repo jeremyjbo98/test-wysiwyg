8e5e1b73bf793ba6d3cf77be32085829055f97cb
Jeremy Ballesteros <jeremy.ballesteros@omni.pro>
2025-02-10 10:20:49 -0500

    Test carousel

--- blocks/carrusel-custom/carrusel-custom.js
+++ blocks/carrusel-custom/carrusel-custom.js
@@ -1,7 +1,5 @@
 // Decorate custom-carrusel block
 export default function decorate(block) {
-    const [carruselWrapper] = block.children;
-    const carrusel = document.createElement('div');
-    carrusel.innerHTML = carruselWrapper.innerHTML.trim();
-    carruselWrapper.replaceChildren(carrusel);
+    const [carouselWrapper] = block.children;
+    console.log("carouselwrapper",carouselWrapper);
 }
