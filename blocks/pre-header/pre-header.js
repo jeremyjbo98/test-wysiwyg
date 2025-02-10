c1cca88980e60ba7bdb0e079262703b9a94f6cda
JTorresV17 <juan.torres@omni.pro>
2025-02-10 07:39:36 -0500

    agregar block pre-header

---
+++ blocks/pre-header/pre-header.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const preHeader = document.createElement("div");
+    preHeader.classList.add("pre-header");
+
+    preHeader.innerHTML = `
+        <a href="#"><i class="fas fa-map-marker-alt"></i> Cochez Vía España <span class="sucursales">Sucursales</span></a>
+        <a href="#"><i class="fas fa-truck"></i> Cupón de $20 por compras de $200</a>
+        <a href="#">Rastrear mi orden</a>
+        <a href="#">Consultar Puntos Gordos</a>
+        <a href="#">Servicios</a>
+        <a href="#" class="business">Empresas</a>
+    `;
+
+    document.body.prepend(preHeader);
+});
