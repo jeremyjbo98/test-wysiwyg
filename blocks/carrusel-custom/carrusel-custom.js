c0acb3863732ea6444aff9a2132052e3addbefae
Jeremy Ballesteros <jeremy.ballesteros@omni.pro>
2025-02-10 10:36:59 -0500

    Test carousel

--- blocks/carrusel-custom/carrusel-custom.js
+++ blocks/carrusel-custom/carrusel-custom.js
@@ -1,4 +1,9 @@
 // Decorate custom-carrusel block
 export default function decorate(block) {
     console.log('Decorating custom-carrusel block', block);
+    [...block.children].forEach((row, index) => {
+
+        console.log(`div numero ${index + 1}:`, row);
+
+    });
 }
