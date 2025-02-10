e75f142142c08d4911c0098379d216aa954509e1
Jeremy Ballesteros <jeremy.ballesteros@omni.pro>
2025-02-10 11:06:50 -0500

    Test carousel

--- blocks/carrusel-custom/carrusel-custom.js
+++ blocks/carrusel-custom/carrusel-custom.js
@@ -16,7 +16,7 @@ export default function decorate(block) {
         });
         bullets.append(bullet);
     });
-
+    block.append(bullets);
     function doSlide(index) {
         console.log('doSlide', index);
     }
