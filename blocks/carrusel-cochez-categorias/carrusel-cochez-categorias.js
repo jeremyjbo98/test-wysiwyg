063504029b4e57f5f03de895b5345aef86b87b3c
Jeremy Ballesteros <jeremy.ballesteros@omni.pro>
2025-02-11 09:36:54 -0500

    Bloque categorias

--- blocks/carrusel-cochez-categorias/carrusel-cochez-categorias.js
+++ blocks/carrusel-cochez-categorias/carrusel-cochez-categorias.js
@@ -1,4 +1,11 @@
 // Decorate cochez-carrusel block
 export default function decorate(block) {
     console.log('Decorating block categories', block);
+
+    block.className = 'carrusel-cochez-categorias-block';
+    [...block.children].forEach((row) => {
+        row.className = 'carrusel-cochez-categorias-item';
+        [...row.children][0].className = 'carrusel-cochez-categorias-item-image';
+        [...row.children][1].className = 'carrusel-cochez-categorias-item-title';
+    });
 }
